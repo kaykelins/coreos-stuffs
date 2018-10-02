0bbd384753b23fb710435d642c436bf612a09936
Kayke Lins <kaykelins@gmail.com>
2018-10-02 12:50:19 -0300

    coreos tests
    initial tests to install coreos

---
+++ coreos-req.sh
@@ -0,0 +1,4 @@
+wget https://github.com/coreos/container-linux-config-transpiler/releases/download/v0.9.0/ct-v0.9.0-x86_64-unknown-linux-gnu
+wget https://raw.githubusercontent.com/coreos/init/master/bin/coreos-install
+chmod +x coreos-install
+chmod +x ct-v0.9.0-x86_64-unknown-linux-gnu
